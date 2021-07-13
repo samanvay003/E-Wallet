50ab5cb434960bf1e5863ef45e7d80cea4535742
Samanvay Srivastava <31175288+samanvay003@users.noreply.github.com>
2021-07-14 05:08:06 +0530

    Create UserRepository.java

---
+++ UserService/src/main/java/com/example/UserService/Repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.user_service.Repository;
+
+
+import com.example.user_service.Model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+}
