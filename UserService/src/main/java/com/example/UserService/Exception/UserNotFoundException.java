7c87cc6dc03c4fa870e96994052d703012c2858b
Samanvay Srivastava <31175288+samanvay003@users.noreply.github.com>
2021-07-14 05:04:03 +0530

    Add files via upload

---
+++ UserService/src/main/java/com/example/UserService/Exception/UserNotFoundException.java
@@ -0,0 +1,10 @@
+package com.example.user_service.Exception;
+
+
+public class UserNotFoundException extends RuntimeException {
+
+    public UserNotFoundException(int id) {
+        super("User id not found : " + id);
+    }
+
+}
