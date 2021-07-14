2f7024eb45f43af793331f1c6a783948d7f7f9a3
Samanvay Srivastava <31175288+samanvay003@users.noreply.github.com>
2021-07-14 05:32:20 +0530

    Add files via upload

---
+++ WalletService/src/main/java/com/example/wallet_service/Exception/WalletBadRequest.java
@@ -0,0 +1,10 @@
+package com.example.wallet_service.Exception;
+
+
+
+public class WalletBadRequest extends RuntimeException {
+    public WalletBadRequest() {
+
+        super("WalletBadRequest " );
+    }
+}
