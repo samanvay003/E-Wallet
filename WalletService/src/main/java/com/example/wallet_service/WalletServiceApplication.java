8f09f527974e82d367fa2e39a3dd3a6683b2dd78
Samanvay Srivastava <31175288+samanvay003@users.noreply.github.com>
2021-07-14 05:41:05 +0530

    Update WalletServiceApplication.java

--- WalletService/src/main/java/com/example/wallet_service/WalletServiceApplication.java
+++ WalletService/src/main/java/com/example/wallet_service/WalletServiceApplication.java
@@ -1,1 +1,16 @@
 
+package com.example.wallet_service;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+public class WalletServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WalletServiceApplication.class, args);
+    }
+
+}
